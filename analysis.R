828a4a6d80095d1c6f9d74c479da861519076856
Nick McKay <nickmckay@users.noreply.github.com>
2018-02-22 18:12:16 -0700

    first batch of figures done

--- analysis.R
+++ analysis.R
@@ -2,7 +2,8 @@
 library(lipdR)
 library(geoChronR)
 library(here)
-
+library(changepoint)
+library(tidyverse)
 #and functions
 source(here("syncFunctions.R"))
 
@@ -24,6 +25,7 @@ for(i in 1:length(D)){
         ye = D[[i]]$chronData[[1]]$model[[1]]$ensembleTable[[1]]$yearEnsemble
         ye$variableName <- "ageEnsemble"
         ye$units <- "BP"
+        ye$values <- 1950- ye$values
         D[[i]]$paleoData[[1]]$measurementTable[[1]]$ageEnsemble <- ye
       }else{
         print(paste("no ensemble data for",D[[i]]$dataSetName))
@@ -40,12 +42,10 @@ varNames = sapply(TS,"[[","paleoData_variableName")
 sapply(TS,"[[","tableType")
 
 
-vars2Analyze = c("Temp","temp2s","accumulation","d18O","seaIceCover","EpsilonC29-C23","NU.MAATEnsemble","AccRate","TTL CARB")
+vars2Analyze = c("Temp","temp2s","accumulation","d18O","seaIceCover","EpsilonC29-C23","Sun2011MAAT","AccRate","TTL CARB")
 sTS = TS[varNames %in% vars2Analyze]
-sapply(sTS,"[[","dataSetName")
-
-
-
+dsn = sapply(sTS,"[[","dataSetName")
+#Data ready to analyze!
 
 # Sea ice records:
 #   CC04.Gibb.2015: sea ice in the labrador sea. 14C age model. Standard marine reservoir correction (Delta R is zero)
@@ -53,13 +53,6 @@ sapply(sTS,"[[","dataSetName")
 # LS009.Ledu.2010: sea ice in Lancaster Sound.  14C age model. Delta R is 400. 14C only covers the early Holocene (so we’re fine for this study). I couldn’t figure out how to add extra “calibrated age” columns, which will eventually include the paleomagnetic tie points. I didn’t bother since these don’t affect the Early Holocene. Let me know if you’d like that info.
 # It’d be great to compare each of the sea ice records with our aquatic leaf wax d2H, epsilon, and temperature records.
 # 
-
-
-
-
-
-
-
 # Ice core records:
 #   GISP2.Kobashi.2017: temp, age. Maximum counting error for the Early Holocene is 2% (Vintner et al. 2006, JGR)
 # GISP2.Stuiver.1995: d18O, age, depth. Would be nice to figure out how to put this on the same age model as Kobashi, although Kobashi doesn’t give us depth, so I’m not totally sure how to approach that…maybe there’s a version of this record in Iso2k on the GICC05 age model, which is what Kobashi used?
@@ -67,3 +60,78 @@ sapply(sTS,"[[","dataSetName")
 # It’d be nice to compare the GISP2 temperature data to our temperature record.
 # Also would be nice to compare NEEM accumulation to our epsilon record
 # All three of these ice core records are on (or in the case of Stuiver 1995 should be on) the GICC05 scale. For this age model, maximum counting error for the Early Holocene is 2% (Vinther et al. 2006, JGR)
+
+Q <- c(4,3,3,3,5,3,3,3,3,4,3,3,3)
+                   #meese.          #andersen are problematic. Andersen is too short and Meese is missing the ensemble data. 
+
+pen = c(.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.01,.1,.1)
+#cpt testing.
+
+time.range <- c(7000,9500)
+
+
+for(i in c(1:5,7,9:13)){
+y = sTS[[i]]$paleoData_values
+if(grepl("epsilon",sTS[[i]]$paleoData_variableName,ignore.case = T)){
+ y[y < -20] <- NA
+}
+ae = sTS[[i]]$ageEnsemble
+med.age <- apply(ae,1,median)
+good = which(!is.na(y) & between(med.age,min(time.range),max(time.range)))
+y <- y[good]
+ae <- ae[good,]
+
+plot(ae[,1],y,main = dsn[i],type="l")
+cp <- cpt.mean(y,method = "SegNeigh",penalty="Manual",pen.value = pen[i],Q=Q[i])
+plot(cp)
+
+#get change point indices..
+cps <- cp@cpts
+if(length(cps)==3){#found two changepoints.
+  cpts <- cps[1:2]
+}else if(length(cps)==4){
+  cpts <- cps[2:3]
+}
+
+#average rows
+cp1 <- colMeans(ae[c(cpts[1],cpts[1]+1),])
+cp2 <- colMeans(ae[c(cpts[2],cpts[2]+1),])
+
+cp1.med = median(cp1)
+cp2.med = median(cp2)
+
+
+tsPlot <- plotTimeseriesEnsRibbons(ae,y,colorLow = "LightGray",colorHigh = "DarkGray") +
+  scale_x_reverse(name = "Age (yr BP)",limits = time.range[order(-time.range)] ) + 
+  ylab(paste0(sTS[[i]]$paleoData_variableName," (",sTS[[i]]$paleoData_units,")")) +
+  geom_vline(aes(xintercept = cp2.med),color = "red")+
+  geom_vline(aes(xintercept = cp1.med),color = "blue")+
+  ggtitle(sTS[[i]]$dataSetName)
+
+cpHist <- ggplot()+geom_density(aes(x = cp2, fill = "Onset"), alpha = 0.5, colour = NA) +
+  geom_density(aes(x = cp1, fill = "Termination"), alpha = 0.5, colour = NA) +
+  scale_x_reverse(name = "Age (yr BP)",limits = time.range[order(-time.range)] )+
+  theme_bw()+scale_fill_brewer("Changepoint",palette = "Set1")+
+  ylab("Probability Density")+
+  theme(legend.position = c(0.1, 0.7))+
+geom_vline(aes(xintercept = cp2.med),color = "red")+
+  geom_vline(aes(xintercept = cp1.med),color = "blue")
+
+p1 <- ggplot_gtable(ggplot_build(tsPlot))
+p2 <- ggplot_gtable(ggplot_build(cpHist))
+
+maxWidth = unit.pmax(p1$widths[2:3], p2$widths[2:3])
+p1$widths[2:3] <- maxWidth
+p2$widths[2:3] <- maxWidth
+finalFig <- grid.arrange(p1, p2, heights = c(3, 2))
+
+ggsave(finalFig,filename = here("figures",paste0(dsn[i],"-Timeseries&changepoints.pdf")))
+
+}
+
+
+
+ensCoevalProb(cp1Age,cp2Age,thresh= 1000)
+
+
+
--- syncFunctions.R
+++ syncFunctions.R
@@ -1,1 +1,46 @@
+
+
+#Define function to test for probability of age uncertain event synchroneity:
+#Let's calculate the probability that two events are simultaneous within some threshold
+ensCoevalProb = function(e1,e2,thresh=10,maxEns=1000){
+  #input is two vectors of samples from a distribution
+  e1 = c(na.omit(e1))#force to vector
+  e2 = c(na.omit(e2))#force to vector
+  if(is.na(maxEns)){
+    maxEns1 = length(e1)
+    maxEns2 = length(e2)
+  }else{
+    maxEns1 = min(c(maxEns,length(e1)))
+    maxEns2 = min(c(maxEns,length(e2)))
+  }
+  #randomly select maxEns elements
+  e1 = e1[sample.int(length(e1),size=maxEns1)]
+  e2 = e2[sample.int(length(e2),size=maxEns2)]
+  
+  
+  #force the two samples to be the same size be resampling the smaller 1.
+  mediff = maxEns1-maxEns2
+  
+  if(mediff<0){
+    #then e1 is shorter
+    e1 = c(e1,e1[sample.int(length(e1),size=abs(mediff))])
+  }else if(mediff>0){
+    #then e2 is shorter
+    e2 = c(e2,e2[sample.int(length(e2),size=abs(mediff))])
+  }
+  
+  ediff = e1-e2
+  outcome = matrix(0,nrow = length(ediff))
+  for(e in 1:length(ediff)){
+    if(abs(ediff[e])<=thresh){
+      outcome[e]=1
+    }
+  }
+  
+  prob = mean(outcome)
+  return(prob)
+}
+
+
+
 #
